972f2111a873cb477328ebc8653d8ea3029c9141
Lukas Cedronas <lukascedronas1@gmail.com>
2018-04-02 23:56:40 +0300

    spring boot initial commit

---
+++ src/main/java/lt/vu/PSKSavana/PskSavanaApplication.java
@@ -0,0 +1,12 @@
+package lt.vu.PSKSavana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PskSavanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PskSavanaApplication.class, args);
+	}
+}
---
+++ src/test/java/lt/vu/PSKSavana/PskSavanaApplicationTests.java
@@ -0,0 +1,16 @@
+package lt.vu.PSKSavana;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PskSavanaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
