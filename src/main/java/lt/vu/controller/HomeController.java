3c8f192c172aac2c722549697be4cb5befc7e18e
Paulius <paulius.strategeens@gmail.com>
2018-04-21 22:11:51 +0300

    Integrate Spring security authentication & Separate controllers

--- src/main/java/lt/vu/controller/HomeController.java
+++ src/main/java/lt/vu/controller/HomeController.java
@@ -24,8 +24,6 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    private Path path;
-
     @Autowired
     private ProductDao productDao;
 
@@ -48,118 +46,5 @@ public class HomeController {
         model.addAttribute(product);
 
         return "product";
-    }
-
-    @RequestMapping("/admin")
-    public String adminPage() {
-        return "admin";
-    }
-
-    @RequestMapping("/admin/productInventory")
-    public String productInventory(Model model) {
-        List<Product> products = productDao.getProducts();
-        model.addAttribute("products", products);
-
-        return "productInventory";
-    }
-
-    @RequestMapping("/admin/productInventory/addProduct")
-    public String addProduct(Model model) {
-        Product product = new Product();
-
-        product.setCategory("Other");
-        product.setCondition("New");
-        product.setStatus("Active");
-
-        model.addAttribute("product", product);
-
-        return "addProduct";
-    }
-
-
-    @RequestMapping(value = "/admin/productInventory/addProduct", method = RequestMethod.POST)
-    public String addProductPost(@Valid @ModelAttribute("product") Product product, BindingResult result,
-                                 HttpServletRequest request) {
-        if (result.hasErrors()) {
-            return "addProduct";
-        }
-
-        productDao.addProduct(product);
-
-        MultipartFile image = product.getImage();
-        String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-        path = Paths.get(rootDirectory + File.separator + "WEB-INF"
-                + File.separator + "resources"
-                + File.separator +"images"
-                + File.separator + product.getId() + ".png");
-
-        if (image != null && !image.isEmpty()) {
-            try {
-                image.transferTo(new File(path.toString()));
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new RuntimeException("Product image saving failed!", e);
-            }
-        }
-
-        return "redirect:/admin/productInventory";
-    }
-
-    @RequestMapping("/admin/productInventory/deleteProduct/{id}")
-    public String deleteProduct(@PathVariable Integer id, Model model, HttpServletRequest request) {
-        productDao.deleteProduct(id);
-
-        String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-        path = Paths.get(rootDirectory + File.separator + "WEB-INF"
-                + File.separator+ "resources"
-                + File.separator + "images"
-                +File.separator + id + ".png");
-
-        if (Files.exists(path)) {
-            try {
-                Files.delete(path);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return "redirect:/admin/productInventory";
-    }
-
-    @RequestMapping("/admin/productInventory/editProduct/{id}")
-    public String editProduct(@PathVariable("id") Integer id, Model model) {
-        Product product = productDao.getProductById(id);
-        model.addAttribute(product);
-
-        return "editProduct";
-    }
-
-    @RequestMapping(value = "/admin/productInventory/editProduct", method = RequestMethod.POST)
-    public String editProduct(@Valid @ModelAttribute("product") Product product, BindingResult result,
-                              Model model, HttpServletRequest request) {
-        if (result.hasErrors()) {
-            return "editProduct";
-        }
-
-        MultipartFile productImage = product.getImage();
-
-        String rootDirectory =  request.getSession().getServletContext().getRealPath("/");
-        path = Paths.get(rootDirectory + File.separator + "WEB-INF"
-                + File.separator + "resources"
-                + File.separator + "images"
-                + File.separator + product.getId() + ".png");
-
-        if (productImage != null && !productImage.isEmpty()) {
-            try {
-                productImage.transferTo(new File(path.toString()));
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new RuntimeException("Product image saving failed!");
-            }
-        }
-
-        productDao.editProduct(product);
-
-        return "redirect:/admin/productInventory";
     }
 }
--- src/main/webapp/WEB-INF/view/admin.jsp
+++ src/main/webapp/WEB-INF/view/admin.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ include file="common/header.jsp"%>
 
 <div class="container-wrapper mt-4">
@@ -7,14 +8,20 @@
             <p class="lead">This is only for administrator!</p>
         </div>
 
+        <c:if test="${pageContext.request.userPrincipal.name != null}">
+            <h2>
+                Welcome: ${pageContext.request.userPrincipal.name} |
+                <%--<a href="<c:url value="/logout"/>">Logout</a>--%>
+                <form:form action="${pageContext.request.contextPath}/logout" method="POST">
+                    <input type="submit" value="Logout" class="btn-link text-info"/>
+                </form:form>
+            </h2>
+        </c:if>
+
         <h3>
             <a href="<c:url value="/admin/productInventory"/>" >Product Inventory</a>
         </h3>
 
-        <p>
-            Here you can view, check and modify product inventory</p>
-        </p>
-
-
+        <p>Here you can view, check and modify product inventory</p>
 
 <%@ include file="common/footer.jsp"%>
