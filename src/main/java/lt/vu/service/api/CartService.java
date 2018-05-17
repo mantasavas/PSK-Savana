e9ac8a44ffef98d138d0129fbd79a772fbae1b00
Tadas <vtadas25@gmail.com>
2018-05-17 17:07:45 +0300

    Create cart when first item is added

--- src/main/java/lt/vu/service/api/CartService.java
+++ src/main/java/lt/vu/service/api/CartService.java
@@ -1,10 +1,13 @@
 package lt.vu.service.api;
 
 import lt.vu.model.Cart;
+import lt.vu.model.Customer;
 
 public interface CartService {
 
     Cart getCartById(int cartId);
 
     void update(Cart cart);
+
+    Cart createNew(Customer customer);
 }
